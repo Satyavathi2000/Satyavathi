71dbe36b00245a7b0ea0b42fa0aaf2cd2399f32c
Satyavathi2000 <oecsatyavathi@gmail.com>
2024-01-12 10:05:59 -0600

    subwing created

---
+++ Subwing.java
@@ -0,0 +1,28 @@
+class subwing {
+	    private double price;
+	     // Getter and setter methods for price
+                   public double getPrice() {
+	           return price;
+                 }
+                     public void setPrice(double price) {                                             this.price = price;
+		}
+
+		// Overloaded method for price;
+		public double getPrice(int quantity){
+		// Calculate price based on quantity
+		      return price * quantity;
+		       public class Main {
+                   public static void main(String[] args) {
+			   // Create an instance of SubDepartment
+        SubDepartment subDepartment = new SubDepartment();
+        subDepartment.setSize(50);
+	Department departmentRef = subDepartment; // Reference of abstract type pointing to a subclass object
+        System.out.println("Department Size: " + departmentRef.getDepartmentSize())
+
+
+		// instace creation
+		Product product = new Product();
+        product.setPrice(10.0);
+
+		   }
+		       }
