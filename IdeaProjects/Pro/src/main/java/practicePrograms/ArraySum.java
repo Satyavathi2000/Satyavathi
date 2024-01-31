e8670096f4d77f8239d4f0fec811c277c2214a35
Satyavathi2000 <oecsatyavathi@gmail.com>
2024-01-31 12:55:55 -0600

    pp

---
+++ IdeaProjects/Pro/src/main/java/practicePrograms/ArraySum.java
@@ -0,0 +1,18 @@
+package practicePrograms;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        // Define an array with some elements
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Calculate the sum of array elements
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        // Print the sum
+        System.out.println("Sum of array elements is: " + sum);
+    }
+}
+
