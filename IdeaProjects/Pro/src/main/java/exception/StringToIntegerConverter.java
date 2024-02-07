310f96135a3afbb862c6a575ed7fed9040271335
Satyavathi2000 <oecsatyavathi@gmail.com>
2024-02-06 20:49:55 -0600

    Converter

--- IdeaProjects/Pro/src/main/java/exception/StringToIntegerConverter.java
+++ IdeaProjects/Pro/src/main/java/exception/StringToIntegerConverter.java
@@ -1,4 +1,4 @@
-package exceptionTest;
+package exception;
 
 public class StringToIntegerConverter {
 
--- IdeaProjects/Pro/src/test/java/exceptionTest/StringToIntegerConverterTest.java
+++ IdeaProjects/Pro/src/test/java/exceptionTest/StringToIntegerConverterTest.java
@@ -1,6 +1,7 @@
 package exceptionTest;
 
 
+import exception.StringToIntegerConverter;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
